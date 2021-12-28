5ba05d160edfa690554a3c88377d1cda552e7a35
cjuillard <claire.juillard.ext@esante.gouv.fr>
2021-12-28 08:56:47 +0100

    Squelette 3 POMs : Spring boot 2.6.2 + dependence pour le modèle

---
+++ psc-maj-ui/src/main/java/fr/ans/psc/model/PsAndStructure.java
@@ -0,0 +1,13 @@
+package fr.ans.psc.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PsAndStructure {
+
+	private Ps ps;
+
+    private Structure structure;
+}
